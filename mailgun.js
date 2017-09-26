4f046a12840d3dc7f76e827b0325e92ef5b65a29
DHLAP\h <vdavidhammond@gmail.com>
2017-09-25 19:24:58 -0600

    Added in enviroment var.

--- mailgun.js
+++ mailgun.js
@@ -1,5 +1,5 @@
-var key = require('./keys.js');
+// var key = require('./keys.js');
 // console.log(key.API_MAIL)
-var mailgun = require('mailgun-js')({apiKey: key.API_MAIL, domain: key.domain});
+var mailgun = require('mailgun-js')({apiKey: key.API_MAIL || process.env.HIDDEN_KEY, domain: key.domain || process.env.domain});
 
 module.exports = mailgun;
