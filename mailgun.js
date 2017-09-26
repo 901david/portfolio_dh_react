52a9927ec6f7e41547af777fc00be2d5d59cc751
DHLAP\h <vdavidhammond@gmail.com>
2017-09-25 19:32:35 -0600

    removed key reference

--- mailgun.js
+++ mailgun.js
@@ -1,5 +1,7 @@
 // var key = require('./keys.js');
 // console.log(key.API_MAIL)
-var mailgun = require('mailgun-js')({apiKey: key.API_MAIL || process.env.HIDDEN_KEY, domain: key.domain || process.env.domain});
+var mailgun = require('mailgun-js')({apiKey: process.env.HIDDEN_KEY, domain: process.env.domain});
 
 module.exports = mailgun;
+
+// ({apiKey: key.API_MAIL || process.env.HIDDEN_KEY, domain: key.domain || process.env.domain})
