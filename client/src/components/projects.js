ac62d0d657b9570b95956757660c0286e5226f00
DHLAP\h <vdavidhammond@gmail.com>
2017-09-26 20:16:18 -0600

    Fixed port image and added margin on title

--- client/src/components/projects.js
+++ client/src/components/projects.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import Modal from 'react-modal';
 
 class IndividProj extends Component {
-          constructor() {
-            super();
+          constructor(props) {
+            super(props);
 
             this.state = {
               modalIsOpen: false
