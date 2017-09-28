6423d6e4f95052ffbde937426b9a6a16105909e2
DHLAP\h <vdavidhammond@gmail.com>
2017-09-28 14:52:18 -0600

    Think I found path error

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
+const favicon = require('serve-favicon')
 
 // Sets up the Express App
 // =============================================================
@@ -14,7 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 require("./routes.js")(app);
 
