2601cc40babdf96cd8fcf6712cc243ceb2ea6dbd
901david <vdavidhammond@gmail.com>
2019-05-02 19:25:16 -0600

    moved build adjusted path to index.html

--- Routes/html.js
+++ Routes/html.js
@@ -2,7 +2,7 @@ const htmlRouter = require("express").Router();
 const path = require("path");
 
 htmlRouter.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join("build", "index.html"));
 });
 
 module.exports = htmlRouter;
