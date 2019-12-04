52b64812f4467fa21dfca0d5b042021cf969af4b
901david <vdavidhammond@gmail.com>
2019-12-04 13:40:27 -0700

    updated server files

--- db-server/server.js
+++ db-server/server.js
@@ -1,7 +1,9 @@
-// require("dotenv").config();
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 const express = require("express");
 const mongoose = require("mongoose");
-const PORT = 8050;
+const PORT = process.env.PORT || 8050;
 const app = express();
 const dataRouter = require("./Routes/data");
 
